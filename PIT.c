e8aaf47ce2f5f899bcbd5405b942a484f190176d
Prangon Das <lkerneld@gmail.com>
2019-07-07 17:24:10 -0400

    PIT interrupt fix

--- PIT.c
+++ PIT.c
@@ -11,29 +11,31 @@ int timer = 0;
 void PIT_setStrtVal(int periodms){
 	switch (periodms) {
 	case 100:
-		PIT->CHANNEL[0]->LDVAL = PIT_PERIOD_100ms;
+		PIT->CHANNEL[0].LDVAL = PIT_PERIOD_100ms;
 		break;
 	case 1000:
-		PIT->CHANNEL[0]->LDVAL = PIT_PERIOD_1000ms;
+		PIT->CHANNEL[0].LDVAL = PIT_PERIOD_1000ms;
 		break;
 	case 10000:
-		PIT->CHANNEL[0]->LDVAL = PIT_PERIOD_10000ms;
+		PIT->CHANNEL[0].LDVAL = PIT_PERIOD_10000ms;
 		break;
 	}
 }
 
 void PIT_restart(){
-	PIT->CHANNEL[0]->TCTRL = PIT_TCTRL_TIMER_DISABLE;
+	PIT->CHANNEL[0].TCTRL = PIT_TCTRL_TIMER_DISABLE;
 	timer = 0;
-	PIT->CHANNEL[0]->TCTRL = PIT_TCTRL_TIMER_I_EN;
+	PIT->CHANNEL[0].TCTRL = PIT_TCTRL_TIMER_I_EN;
 }
 
 void PIT_IRQHandler(){
-	if(PIT->CHANNEL[0]->TFLG & PIT_TFLG_TIF_MASK){
+	__asm("CPSID I");
+	if(PIT->CHANNEL[0].TFLG & PIT_TFLG_TIF_MASK){
 		timer++;
 		/* Writing one to it clears interrupt for next one */
-		PIT->CHANNEL[0]->TFLG = PIT_TFLG_TIF_MASK;
+		PIT->CHANNEL[0].TFLG = PIT_TFLG_TIF_MASK;
 	}
+	__asm("CPSIE I");
 }
 
 int PIT_readTime(){
@@ -42,7 +44,7 @@ int PIT_readTime(){
 
 void PIT_start(){
 	/* Enable PIT interrupt for channel/timer 0 and start timer*/
-	PIT->CHANNEL[0]->TCTRL = PIT_TCTRL_TIMER_I_EN;
+	PIT->CHANNEL[0].TCTRL = PIT_TCTRL_TIMER_I_EN;
 }
 
 void init_PIT_IRQ(int periodms){
@@ -50,11 +52,17 @@ void init_PIT_IRQ(int periodms){
 	/* Enable clock for PIT */
 	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
 
-	/* Enable PIT module with freeze during debugging */
-	PIT->MCR = PIT_MCR_MDIS_FRZ_EN_MASK;
+	/* Set PIT interrupt priority */
+	NVIC->IP[PIT_NVIC_IPR_REGISTER] |= NVIC_IPR_PIT_MASK;
+	/* Clear any pending PIT interrupts */
+	NVIC->ICPR[0] = NVIC_ICPR_PIT_MASK;
+	/* Unmask PIT interrupts */
+	NVIC->ISER[0] = NVIC_ISER_PIT_MASK;
 
 	/* Initialize timer value */
 	PIT_setStrtVal(periodms);
+	/* Enable PIT module with freeze during debugging */
+	PIT->MCR = PIT_MCR_MDIS_FRZ_EN_MASK;
 }
 
 
--- UART0.h
+++ UART0.h
@@ -1,14 +1,15 @@
 #ifndef UART_0
 #define UART_0
 
-#define NVIC_ICPR_UART0_MASK (1 << 12)
+#define UART0_NVIC_IRQ		(12)
+#define NVIC_ICPR_UART0_MASK 	(1 << UART0_NVIC_IRQ)
 
-#define NVIC_ISER_UART0_MASK (1 << 12)
+#define NVIC_ISER_UART0_MASK 	(1 << UART0_NVIC_IRQ)
 
-#define UART0_IPR_REGISTER (12 >> 2)
+#define UART0_IPR_REGISTER 	(UART0_NVIC_IRQ >> 2)
 
 #define NVIC_IPR_UART0_MASK \
-                      (3 << (((12 & 3) << 3) + 6))
+                      (3 << (((UART0_NVIC_IRQ & 3) << 3) + 6))
 
 #define PORT_PCR_SET_PTA_UART0_TR (PORT_PCR_ISF_MASK | \
                                     (2u << PORT_PCR_MUX_SHIFT))
@@ -21,23 +22,23 @@
                                    SIM_SOPT5_UART0RXSRC_MASK | \
                                    SIM_SOPT5_UART0TXSRC_MASK)
 
-#define UART0_BDH_9600  (0x01u)
+#define UART0_BDH_9600  	(0x01u)
 
-#define UART0_BDL_9600  (0x38u)
+#define UART0_BDL_9600  	(0x38u)
 
-#define UART0_C1_8N1  (0x00)
+#define UART0_C1_8N1  		(0x00)
 
-#define UART0_C2_T_R    (UART0_C2_TE_MASK | UART0_C2_RE_MASK)
-#define UART0_C2_T_RI   (UART0_C2_T_R | UART0_C2_RIE_MASK)
-#define UART0_C2_TI_RI  (UART0_C2_T_RI | UART0_C2_TIE_MASK)
+#define UART0_C2_T_R    	(UART0_C2_TE_MASK | UART0_C2_RE_MASK)
+#define UART0_C2_T_RI   	(UART0_C2_T_R | UART0_C2_RIE_MASK)
+#define UART0_C2_TI_RI  (	UART0_C2_T_RI | UART0_C2_TIE_MASK)
 
-#define UART0_C3_NO_TXINV  (0x00)
+#define UART0_C3_NO_TXINV  	(0x00)
 
 #define UART0_C4_NO_MATCH_OSR_16  (0x0Fu)
 
 #define UART0_C5_NO_DMA_SSR_SYNC  (0x00)
 
-#define UART0_S1_CLEAR_FLAGS  (0x1Fu)
+#define UART0_S1_CLEAR_FLAGS  	(0x1Fu)
 
 #define UART0_S2_NO_RXINV_BRK10_NO_LBKDETECT_CLEAR_FLAGS  (0xC0u)
 
